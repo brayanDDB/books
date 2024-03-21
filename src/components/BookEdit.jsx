ece3b93d62b1be8a4fdb7fe80856113c8719ef79
Brayan Guerrero <brayan.guerrero@coxautoinc.com>
2024-03-20 21:28:11 -0500

    COMMIT using custom hook

--- src/components/BookEdit.jsx
+++ src/components/BookEdit.jsx
@@ -1,9 +1,9 @@
-import { useState, useContext } from "react";
-import BooksContext from "../context/books";
+import { useState } from "react";
+import useBooksContext from "../hooks/use-books-context";
 
 function BookEdit({ book, onSubmit }) {
     const[title, setTitle] = useState(book.title);
-    const { handleEditBook } = useContext(BooksContext);
+    const { handleEditBook } = useBooksContext();
 
     const handleSubmit = (event) => {
         event.preventDefault();
